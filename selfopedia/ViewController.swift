a0ca81be4470a284b5aa8586d55224901f55f28d
lolansanchez <lsanchez@gcschool.org>
2022-06-28 20:44:37 -0400

    quote randomizer v1.0

--- selfopedia/ViewController.swift
+++ selfopedia/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let randQuo = homepageQuotes.randomElement()
+        let quote = randQuo?.key
+        let author = randQuo?.value
+        quoteLabel.text = quote
+        authorLabel.text = author
     }
 
 
---
+++ selfopedia/quoteDictionary.swift
@@ -0,0 +1,9 @@
+//
+//  quoteDictionary.swift
+//  selfopedia
+//
+//  Created by Lola Sanchez on 6/28/22.
+//
+
+import Foundation
+let homepageQuotes = ["You carry the passport to your own happiness.": "Diane von Furstenberg", "It is a rough road that leads to the heights of greatness.": "Lucious Annaeus Seneca", "If we take care of the moments, the years will take care of themselves.": "Maria Edgeworth", "The best way out is always through.": "Robert Frost", "You can be the ripest, juiciest peach in the world, and there's still going to be somebody who hates peaches." : "Dita Von Teese", "Keep a little fire burning; however small, however hidden." : "Cormac McCarthy", "If you don't like the road you're walking, start paving a new one." : "Dolly Parton", "Doubt is a killer. You just have to know who you are and what you stand for." : "Jennifer Lopez", "No one changes the world who isn't obsessed." : "Billie Jean King", "Success is stumbling from failure to failure with no loss of enthusiasm." : "Winston Churchill" ]
