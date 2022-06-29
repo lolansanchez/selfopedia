571929d725a55c9bff5cbde4862ba798e67416e2
lolansanchez <lsanchez@gcschool.org>
2022-06-28 20:59:39 -0400

    empty emotion arrays created

---
+++ selfopedia/feelingLists.swift
@@ -0,0 +1,35 @@
+//
+//  feelingLists.swift
+//  selfopedia
+//
+//  Created by Lola Sanchez on 6/28/22.
+//
+
+import Foundation
+var anxious: [String] = []
+var bored: [String] = []
+var confused: [String] = []
+var sick: [String] = []
+var empatheticPain: [String] = []
+var guilty: [String] = []
+var nostalgic: [String] = []
+var depressed: [String] = []
+var tired: [String] = []
+var unmotivated: [String] = []
+var angry: [String] = []
+var lonely: [String] = []
+var annoyed: [String] = []
+var restless: [String] = []
+var disappointment: [String] = []
+var grief: [String] = []
+var despair: [String] = []
+var grumpy: [String] = []
+var panicked: [String] = []
+var regretful: [String] = []
+var selfConscious: [String] = []
+var stressed: [String] = []
+var jealous: [String] = []
+var homesickness: [String] = []
+var humiliated : [String] =  []
+
+
