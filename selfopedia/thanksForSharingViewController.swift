7ca23e8389e0693a8d31d863efb2b94669802cad
lolansanchez <lsanchez@gcschool.org>
2022-07-01 11:25:07 -0400

    e

--- selfopedia/thanksForSharingViewController.swift
+++ selfopedia/thanksForSharingViewController.swift
@@ -10,24 +10,36 @@ import SwiftUI
 
 
 class thanksForSharingViewController: UIViewController {
-    var componentArray = Array(feelingListDict.keys)
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        for "" in feelingListDict["anxious"] {
-            
-        }
-        }
+  
     
-    
-
-    @IBOutlet weak var cmView: UILabel!
+    @IBOutlet weak var cmView: UITextView!
+    @IBAction func lastDitchEffort(_ sender: Any) {
+        //for item in feelingListDict["anxious"]! {
+        //if selected == "anxious"{
+           // let viewList = feelingListDict["anxious"]?.joined(separator: "/")
+            //cmView.text = viewList
+   // }
+        print(feelingListDict["bored"])
+    }
     
     @IBAction func additionalResources(_ sender: UIButton) {
         if let url = URL(string: "https://docs.google.com/document/d/1JHuP6p-j_NF00erfLST7lhGHTgR9uOYeb00mkXwrjhY/edit?usp=sharing") {
             UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }}
+    var componentArray = Array(feelingListDict.keys)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
         }
     
-
 }
+    
+    
 
-}
+    
+    
+
+    
+    
+    
+
